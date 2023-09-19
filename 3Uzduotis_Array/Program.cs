31ef03b1d954660ef17630dd9f20aac60e2aeb42
Laura <Laura@DESKTOP-BESSB6E>
2023-09-19 18:51:10 +0300

    Add project files.

---
+++ 3Uzduotis_Array/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _3Uzduotis_Array
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] randomNumbers = { 2, 5, 9, 4, 7 };
+            
+
+            for (int i = 0; i < randomNumbers.Length - 1; i++)
+            {
+                for (int j = 0; j < randomNumbers.Length - (1 + i); j++)
+                {
+                    if (randomNumbers[j] > randomNumbers[j + 1])
+                    {
+
+                        int temp = randomNumbers[j + 1];
+                        randomNumbers[j + 1] = randomNumbers[j];
+                        randomNumbers[j] = temp;
+                    }
+                }
+            }
+            for (int k = 0;  k < randomNumbers.Length;  k++)
+            {
+                Console.WriteLine(randomNumbers[k]);
+            }
+        }
+    }
+}
